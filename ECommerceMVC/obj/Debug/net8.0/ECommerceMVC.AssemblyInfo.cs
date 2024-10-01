78c83b73c0e7ab7f43029c5c0afe2b58dd8a2a4f
Khang Nguyễn <nguyenhaankhang@gmail.com>
2024-10-01 15:51:04 +0700

    merge layout template

--- ECommerceMVC/obj/Debug/net8.0/ECommerceMVC.AssemblyInfo.cs
+++ ECommerceMVC/obj/Debug/net8.0/ECommerceMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5132fa7aa10922bfd9ef522d1b457b553e91ef3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
